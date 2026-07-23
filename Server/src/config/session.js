e18503305f35408e517d1b267b5a375e761db416
NeerajDev7 <neeraj2810d@gmail.com>
2026-07-23 22:00:44 +0530

    Debug env

--- Server/src/config/session.js
+++ Server/src/config/session.js
@@ -2,28 +2,34 @@ import session from "express-session";
 import MySQLStore from "express-mysql-session";
 
 export default function configureSession(app) {
+  console.log("DB CONFIG:", {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+  });
 
-    const MySQLSessionStore = MySQLStore(session);
+  const MySQLSessionStore = MySQLStore(session);
 
-    const sessionStore = new MySQLSessionStore({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-    });
+  const sessionStore = new MySQLSessionStore({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
 
-    app.use(
-        session({
-            secret: "My first session",
-            resave: false,
-            saveUninitialized: false,
-            store: sessionStore,
-            cookie: {
-                maxAge: 1000 * 60 * 60 * 24,
-                httpOnly: true,
-                secure: true,
-                sameSite: "none",
-            },
-        })
-    );
+  app.use(
+    session({
+      secret: "My first session",
+      resave: false,
+      saveUninitialized: false,
+      store: sessionStore,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24,
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      },
+    }),
+  );
 }
